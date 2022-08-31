bbbd4cb74eab3a2b1fa57cbc8da8fb683f132d7b
Ayrton Douglas Freitas Guimarães <ayrtondouglas@usp.br>
2022-08-31 14:57:29 -0300

    fix: foi criada uma nova skin ime e foi alterado o footer

---
+++ resources/views/partials/skins/ime/footer.blade.php
@@ -0,0 +1,9 @@
+@section('skin_footer')
+<!-- Footer uspdev -->
+<footer>
+    <div class="text-center py-4">
+        <hr />
+        <a href="https://www.ime.usp.br/" target="_blank">IME-USP</a>
+    </div>
+</footer>
+@endsection
